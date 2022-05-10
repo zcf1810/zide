66ade15232121e3db3a420315fe700f089a97b1a
zhangchunfa <zhangchunfa@xiaomi.com>
2022-05-10 21:58:45 +0800

    bugfix: just start lua lsp in root path of nvim running
    Signed-off-by: zhangchunfa <zhangchunfa@xiaomi.com>

--- lua/configure/sumneko_lua.lua
+++ lua/configure/sumneko_lua.lua
@@ -8,7 +8,8 @@ return {
     log_level = 2,
     -- 再比如我将该服务器的工作域范围改成了当前所在目录的工作区，避免了重复运行多个同样的语言服务器的问题
     root_dir = function()
-        return vim.fn.getcwd()
+        return global_root_path
+        --return vim.fn.getcwd()
     end,
     settings = {
         Lua = {
