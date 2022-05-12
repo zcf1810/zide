7c15ed21f43036404d7f0b777691d5a3e5a76f41
zhangchunfa <zhangchunfa@xiaomi.com>
2022-05-12 10:01:12 +0800

    optimzie: remove gopls
    Signed-off-by: zhangchunfa <zhangchunfa@xiaomi.com>

--- lua/configure/lsp_installer.lua
+++ lua/configure/lsp_installer.lua
@@ -4,11 +4,11 @@ plugin.core = {
     "williamboman/nvim-lsp-installer",
     --as = "nvim-lspinstaller",
     requires = {
-        {"hrsh7th/nvim-cmp"},
-        {"hrsh7th/cmp-nvim-lsp"},
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-nvim-lsp" },
     },
 
-    setup = function()  -- Specifies code to run before this plugin is loaded.
+    setup = function() -- Specifies code to run before this plugin is loaded.
 
     end,
 
@@ -19,8 +19,6 @@ plugin.core = {
         -- 配置：https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
         --
         -- 使用 cmp_nvim_lsp 代替内置 omnifunc，获得更强的补全体验
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
         local servers = {
             -- 语言服务器名称：配置选项
@@ -39,6 +37,11 @@ plugin.core = {
         }
         -- 这里是 LSP 服务启动后的按键加载
         local function attach(_, bufnr)
+            local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+            local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+            -- enable completion trippered by <c-x><c-o>
+            buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
             ---- 跳转到定义（代替内置 LSP 的窗口，telescope 插件让跳转定义更方便）
             --vim.keybinds.bmap(bufnr, "n", "gd", "<cmd>Telescope lsp_definitions theme=dropdown<CR>", vim.keybinds.opts)
             ---- 列出光标下所有引用（代替内置 LSP 的窗口，telescope 插件让查看引用更方便）
@@ -57,32 +60,37 @@ plugin.core = {
             --vim.keybinds.bmap(bufnr, "n", "g]", "<cmd>Lspsaga diagnostic_jump_next<CR>", vim.keybinds.opts)
             ---- 悬浮窗口上翻页，由 Lspsaga 提供
             --vim.keybinds.bmap(
-                --bufnr,
-                --"n",
-                --"<C-p>",
-                --"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>",
-                --vim.keybinds.opts
+            --bufnr,
+            --"n",
+            --"<C-p>",
+            --"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>",
+            --vim.keybinds.opts
             --)
             ---- 悬浮窗口下翻页，由 Lspsaga 提供
             --vim.keybinds.bmap(
-                --bufnr,
-                --"n",
-                --"<C-n>",
-                --"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>",
-                --vim.keybinds.opts
+            --bufnr,
+            --"n",
+            --"<C-n>",
+            --"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>",
+            --vim.keybinds.opts
             --)
+            --
         end
 
         -- 自动安装或启动 LanguageServers
         local lsp_config = require('lspconfig')
+        local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
         for server_name, server_options in pairs(servers) do
             local server_available, server = lsp_installer_servers.get_server(server_name)
             -- 判断服务是否可用
             if server_available then
                 if not server:is_installed() then
-                    vim.notify("Install Language Server : " .. server_name, "WARN", {title = "Language Servers"})
+                    vim.notify("Install Language Server : " .. server_name, "WARN", { title = "Language Servers" })
                     server:install()
                 end
+                server_options.attach = attach;
+                server_options.capabilities = capabilities
                 lsp_config[server_name].setup(server_options)
             end
         end
